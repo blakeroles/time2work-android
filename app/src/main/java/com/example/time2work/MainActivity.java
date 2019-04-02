e333fa1dfeb03187b7244bc71aa0f50795422717
Blake Roles <blakeroles@gmail.com>
2019-04-02 18:06:08 +1100

    Added Model which captures the UI information. Instantiated a model when clicking the save button.

--- app/src/main/java/com/example/time2work/MainActivity.java
+++ app/src/main/java/com/example/time2work/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.time2work;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declaring variables from the UI
+    private EditText homeAddress;
+    private EditText workAddress;
+    private EditText timeToArriveAtWork;
+    private EditText timeToGetReady;
+    private Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view)
+    {
+        // Switch statement to determine what has been clicked
+        switch ((view).getId())
+        {
+            case R.id.button:
+                Model newModel = new Model(workAddress.toString(), homeAddress.toString(), Integer.parseInt(timeToArriveAtWork.toString()), Integer.parseInt(timeToGetReady.toString()));
+        }
     }
 }
