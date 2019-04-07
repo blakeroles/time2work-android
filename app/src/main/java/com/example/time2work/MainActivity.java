8a297fa929afff9f3236dba6a0ef64b6353199cd
Blake Roles <blakeroles@gmail.com>
2019-04-07 14:42:41 +1000

    Added file I/O for saving addresses to local file system.

--- app/src/main/java/com/example/time2work/MainActivity.java
+++ app/src/main/java/com/example/time2work/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.time2work;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,11 @@ import android.widget.TextView;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import org.apache.commons.io.IOUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
     private Button saveButton;
     private Button editButton;
     private Button calculateButton;
+
+    private Model newModel;
+    public static final String FILE_NAME = "time2work.csv";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +45,24 @@ public class MainActivity extends AppCompatActivity {
 
         // Check to see if saved file exists, if it does:
         try {
-            FileInputStream appFile = openFileInput("time2work.csv");
+            FileInputStream appFileStream = openFileInput(FILE_NAME);
 
             // TODO: Fill EditText fields with data from file.
             calculateButton.setEnabled(true);
+            String stringFromFileStream = getFileContent(appFileStream);
+            if (stringFromFileStream.matches(""))
+            {
+                // TODO: Error Occured!
+            } else
+            {
+                // Split the string and populate the fields appropriately
+                System.out.println("I read this text: " + stringFromFileStream);
+                String[] stringFromFileArr = stringFromFileStream.split(",");
+                homeAddress.setText(stringFromFileArr[0]);
+                workAddress.setText(stringFromFileArr[1]);
+                timeToArriveAtWork.setText(stringFromFileArr[2]);
+            }
+
 
         } catch (FileNotFoundException e) {
             // If file does not exist
@@ -59,25 +82,33 @@ public class MainActivity extends AppCompatActivity {
                 disableEditText(workAddress);
                 disableEditText(timeToArriveAtWork);
 
-                System.out.println(homeAddress.isEnabled());
+                // Check that newModel is populated
+                if (workAddress.getText().toString().matches("") && homeAddress.getText().toString().matches("") && timeToArriveAtWork.getText().toString().matches(""))
+                {
+                    // TODO: Add a toast/alert message to populate fields
+                } else
+                {
+                    // Create a model object
+                    newModel = new Model(workAddress.getText().toString(), homeAddress.getText().toString(), Integer.parseInt(timeToArriveAtWork.getText().toString()));
+                    System.out.println("New Model object created with: Home Address = " + homeAddress.getText().toString() + ", Work Address = " + workAddress.getText().toString() + ", Time To Arrive At Work = " + timeToArriveAtWork.getText().toString());
+                }
+
+                // Call saveFile function
+                Boolean result = saveFile(newModel);
+
+                // TODO: Depending on result: set an alert saying if it was successful or not
+
+
+                // TODO: Populate the text fields with the saved information
 
                 break;
 
-                // Create a new model object
-                // Model newModel = new Model(workAddress.toString(), homeAddress.toString(), Integer.parseInt(timeToArriveAtWork.toString()));
-
-
-                // TODO: Call saveFile function
-                // TODO: Populate the text fields with the saved information
-                // TODO: Allow the Edit button to be visible and save button to be invisible
-
-            // TODO: Implement if Edit button is pressed
+            // Implement if Edit button is pressed
             case R.id.buttonEdit:
                 // Unlock all the fields to be editable
                 enableEditText(homeAddress);
                 enableEditText(workAddress);
                 enableEditText(timeToArriveAtWork);
-                System.out.println(homeAddress.isEnabled());
                 break;
 
             case R.id.buttonCalculate:
@@ -91,15 +122,49 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public boolean saveFile()
+    public boolean saveFile(Model m)
     {
+        // Check to see if saved file exists, if it does:
+        try {
+            FileInputStream appFile = openFileInput(FILE_NAME);
+            File appFileToDelete = new File(getFilesDir(), FILE_NAME);
+            appFileToDelete.delete();
+            System.out.println("I deleted the file: " + FILE_NAME);
+
+            writeToFile(m);
+
+
+        } catch (FileNotFoundException e) {
+            // If file does not exist
+            writeToFile(m);
+            e.printStackTrace();
+        }
+
+
+
+
         // TODO: Implement function to save the new Model to the file system
         // TODO: Check if a saved file already exists
         // TODO: If saved file already exists, prompt the user if they want to overwrite
         // TODO:      Delete saved file
-        // TODO: Create and save new file with new Model information
+        // Create and save new file with new Model information
 
         return true;
+    }
+
+    public void writeToFile(Model m)
+    {
+        String fileContents = m.getHomeAddress() + "," + m.getWorkAddress() + "," + m.getTimeToArriveAtWork();
+        FileOutputStream outputStream;
+
+        try {
+            outputStream = openFileOutput(FILE_NAME, Context.MODE_PRIVATE);
+            System.out.println("File contents: " + fileContents.getBytes());
+            outputStream.write(fileContents.getBytes("UTF-8"));
+            outputStream.close();
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
     }
 
     public String callGoogleApi()
@@ -128,6 +193,22 @@ public class MainActivity extends AppCompatActivity {
         editText.setFocusableInTouchMode(true);
         editText.setEnabled(true);
         editText.setCursorVisible(true);
+    }
+
+    public String getFileContent( FileInputStream fis)
+    {
+        String fileString;
+        try {
+            StringWriter writer = new StringWriter();
+            IOUtils.copy(fis, writer, "UTF-8");
+            fileString = writer.toString();
+            System.out.println("File is: " + fileString);
+            return fileString;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "";
     }
 
 }
